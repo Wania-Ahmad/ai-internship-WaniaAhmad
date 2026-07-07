acd4362ba2f8a4649faa2020b37ffecebcef8d89
Wania-Ahmad <wania.ahmad31@gmail.com>
2026-07-07 18:34:35 +0500

    Updated bonustask.py

--- bonustask.py
+++ bonustask.py
@@ -1,3 +1,3 @@
-# This is my bonus task for the task 02 for AI Internship.
 name = input("Enter your name: ")
 print(f"Welcome to the AI Internship, {name}!")
+# This is my bonus task for the task 02 for AI Internship.
